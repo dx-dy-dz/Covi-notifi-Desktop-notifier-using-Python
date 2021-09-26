8efb0075647431c866be1f9af4fcd31b53ba5191
dx-dy-dz <72966656+dx-dy-dz@users.noreply.github.com>
2021-09-26 21:53:31 +0530

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -3,7 +3,7 @@ import datetime #for reading present date
 import time 
 import requests #for retreiving coronavirus data from web
 from plyer import notification #for getting notification on your PC
-#let there is no data initially
+#consider that there is no data initially
 covidData = None
 try:
     covidData = requests.get("https://corona-rest-api.herokuapp.com/Api/india")
